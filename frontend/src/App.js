88eeee585738666027a2f8403bf075b59bbd9a98
cristian.orozco <cristian.orozco@globant.com>
2021-04-30 23:54:38 -0500

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import Slider from './components/Slider/Slider'
+import './App.scss'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className='app__container'>
+      <Header />
+      <Slider />
+      <Slider />
+      <Slider />
+    </div>
+  )
+}
+
+export default App
