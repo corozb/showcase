5a3b92479b0cd4cb5a0c22493fcbc7ab11b36e0c
Cristian Orozco <cristian.orozco@globant.com>
2022-12-21 09:28:18 -0500

    Api endpoint render.com

--- frontend/src/services/endpoint.ts
+++ frontend/src/services/endpoint.ts
@@ -1,1 +1,1 @@
-export const API_URL = process.env.REACT_APP_API_URL || 'https://showcase-api-v1.herokuapp.com'
+export const API_URL = 'https://showcase-api-fggc.onrender.com' || process.env.REACT_APP_API_URL
